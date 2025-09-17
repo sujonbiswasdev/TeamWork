e491f1eaefd907d99a5d9e768bf34561d59ef9ee
jonayed-nasir <junayednasir073@gmail.com>
2025-09-17 17:38:00 +0600

    process section done

--- src/pages/Process Page/Process.jsx
+++ src/pages/Process Page/Process.jsx
@@ -2,14 +2,18 @@ import React from 'react'
 import Hero from './compo/Hero'
 import SquareUp from './compo/SquareUp'
 import Overview from './compo/Overview'
+import Interest from './compo/Interest'
+import From from './compo/From'
 
 function Process() {
   return (
     <main className='bg-[#1E1E1E]'>
-      <Hero />
       <div className='w-full max-w-[1596px] mx-auto px-3 md:px-10'>
+      <Hero />
       <SquareUp />
       <Overview />
+      <Interest />
+      <From />
       </div>
     </main>
   )
