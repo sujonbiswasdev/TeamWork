370211cf45c22a313c732c34c7bd0ca080af23f6
sujonbiswasdev <sujonbiswasdev@gmail.com>
2025-09-19 23:40:29 +0600

    main

--- src/pages/About Page/Story.jsx
+++ src/pages/About Page/Story.jsx
@@ -11,8 +11,8 @@ const Story = () => {
       },[])
   return (
     <div>
-        <div className='px-8 py-10'> 
-            <h2 className='text-5xl font-bold'>Our Story</h2>
+        <div className='px-8 py-10 ml-9'> 
+            <h2 className='text-5xl font-bold '>Our Story</h2>
         </div>
         <div className='px-6 py-10 '>
             <div className='grid grid-cols-1 lg:grid-cols-2 gap-8 px-4 py-3'>
