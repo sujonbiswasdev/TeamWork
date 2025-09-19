ac94d6351ab384d4b5a6b53d7f166011a5c2dc77
123-Sweety153 <hiransweety138@gmail.com>
2025-09-19 21:33:39 +0600

    any comment

---
+++ public/data/Switch.js
@@ -0,0 +1,104 @@
+export const CardData = [
+    {
+        Image: "/images/home/home-Design.png",
+        title:"Design",
+        des:"At Squareup, our design team is passionate about creating stunning, user-centric designs that captivate your audience and elevate your brand. We believe that great design is not just about aesthetics; it's about creating seamless and intuitive user experiences.",
+        mybtn:"Learn More"
+    },
+
+    {
+        Image: "/images/home/home-Engineer.png",
+        title:"Engineering",
+        des:"Our engineering team combines technical expertise with a passion for innovation to build robust and scalable digital solutions. We leverage the latest technologies and best practices to deliver high-performance applications tailored to your specific needs.",
+        mybtn:"Learn More"
+    },
+
+    {
+        Image: "/images/home/home-Project Management.png ",
+        title:"Project Management",
+        des:"Our experienced project management team ensures that your projects are delivered on time, within budget, and according to your specifications. We follow industry-standard methodologies and employ effective communication and collaboration tools to keep you informed throughout the development process.",
+        mybtn:"Learn More"
+    },
+]
+
+export const CardData2 = [
+    {
+        Image: "/images/home/Expertise.png",
+        title:"Expertise",
+        des:"Our team consists of highly skilled professionals who have a deep understanding of the digital landscape. We stay updated with the latest industry trends and best practices to deliver cutting-edge solutions.",
+    },
+
+    {
+        Image: "/images/home/Client.png",
+        title:"Client-Centric Approach",
+        des:"At Squareup, our design team is passionate about creating stunning, user-centric designs that captivate your audience and elevate your brand. We believe that great design is not just about aesthetics; it's about creating seamless and intuitive user experiences.",
+    },
+
+    {
+        Image: "/images/home/Results.png",
+        title:"Results-Driven Solutions",
+        des:"Our primary focus is on delivering results. We combine creativity and technical expertise to create digital products that drive business growth, enhance user experiences, and provide a competitive advantage.",
+    },
+
+    {
+        Image: "/images/home/Collaborative.png",
+        title:"Collaborative Partnership",
+        des:"We value long-term relationships with our clients. We see ourselves as your digital partner, providing ongoing support, maintenance, and updates to ensure your digital products continue to thrive.",
+    },
+]
+
+export const CardData3 = [
+    {
+        title:"SquareUp has been Instrumental in Transforming our Online Presence. ",
+        des:"Their team's expertise in web development and design resulted in a visually stunning and user-friendly e-commerce platform. Our online sales have skyrocketed, and we couldn't be happier.",
+        Image:"/images/home/John-Smith.png",
+        title1:"John Smith",
+        des1:"CEO of Chic Boutique",
+        mybtn:"Open Website"
+    },
+
+     {
+        title:"Working with SquareUp was a breeze.",
+        des:"They understood our vision for a mobile app that streamlined our food delivery service. The app they delivered exceeded our expectations, and our customers love the seamless ordering experience. SquareUp is a trusted partner we highly recommend.",
+        Image:"/images/home/Sarah-Johnson.png",
+        title1:"Sarah Johnson",
+        des1:"Founder of HungryBites.",
+        mybtn:"Open Website"
+    },
+
+     {
+        title:"SquareUp developed a comprehensive booking and reservation system for our event management company",
+        des:"Their attention to detail and commitment to delivering a user-friendly platform was evident throughout the project. The system has streamlined our operations and enhanced our clients' event experiences.",
+        Image:"/images/home/Mark-Thompson.png",
+        title1:"Mark Thompsonr",
+        des1:"CEO of EventMasters",
+        mybtn:"Open Website"
+    },
+
+     {
+        title:"ProTech Solutions turned to SquareUp to automate our workflow ",
+        des:"They delivered an exceptional custom software solution. The system has significantly increased our productivity and reduced manual errors. SquareUp's expertise and professionalism have made them a trusted technology partner.",
+        Image:"/images/home/Laura-Adams.png",
+        title1:"Laura Adams",
+        des1:"COO of ProTech Solutions.",
+        mybtn:"Open Website"
+    },
+
+     {
+        title:"SquareUp designed and developed a captivating web portal for showcasing our real estate listings. ",
+        des:"The platform is visually appealing and easy to navigate, allowing potential buyers to find their dream homes effortlessly. SquareUp's expertise in the real estate industry is unmatched.",
+        Image:"/images/home/Michael-Anderson.png",
+        title1:"Michael Anderson",
+        des1:"CEO of Chic Boutique",
+        mybtn:"Open Website"
+    },
+
+     {
+        title:"FitLife Tracker wanted a mobile app that tracked fitness activities and provided personalized workout plans. ",
+        des:"SquareUp's team developed an intuitive and feature-rich app that has helped our users stay motivated and achieve their fitness goals. We highly recommend SquareUp for any health and fitness app development needs.",
+        Image:"/images/home/Emily-Turner.png",
+        title1:"Emily Turner",
+        des1:"CEO of FitLife-Tracker",
+        mybtn:"Open Website"
+    },
+]
