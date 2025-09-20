35c9b37d06793daad19b41fb6434e5471fd6eb13
sujonbiswasdev <sujonbiswasdev@gmail.com>
2025-09-20 17:37:01 +0600

    done

--- src/pages/Home Page/Trusted.jsx
+++ src/pages/Home Page/Trusted.jsx
@@ -1,12 +1,12 @@
-
+import Marquee from "react-fast-marquee";
 const Trusted = () => {
   return (
     <div className=' mx-auto '>
       <div className='flex justify-center items-center -mt-5'>
         <button className='text-sm px-6 py-2 rounded-full text-white border-1 border-gray-700 bg-gray-900 transition'>Trusted By 250+ Companies</button> 
       </div>
-
-        <div className='flex flex-wrap justify-center gap-8 opacity-80'>
+      <Marquee>
+         <div className='flex flex-wrap justify-center gap-8 opacity-80'>
         <img src="/images/home/zapier.png" alt="" />
         <img src="/images/home/spotify.png" alt="" />
         <img src="/images/home/zoom.png" alt="" />
@@ -14,6 +14,8 @@ const Trusted = () => {
         <img src="/images/home/amazon.png" alt="" />
         <img src="/images/home/adobe.png" alt="" />
       </div>  
+      </Marquee>
+       
 
     </div> 
 
